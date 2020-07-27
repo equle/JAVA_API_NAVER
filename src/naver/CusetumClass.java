8b7bb827c506c23dd6333d7407b0189824062adf
802-3 <802-3@172.16.83.115>
2020-07-27 09:27:29 +0900

    검색 가능

---
+++ src/naver/CusetumClass.java
@@ -0,0 +1,14 @@
+package naver;
+
+import java.util.Scanner;
+
+public class CusetumClass {
+
+	public String Search() {
+		System.out.print("검색어를 입력하세요 : ");
+		Scanner sc = new Scanner(System.in);
+		String input = sc.next();
+		return input;
+	}
+	
+}
